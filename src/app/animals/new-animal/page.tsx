fac0f104eede3bac2705f9acee67da6ae0f69dfa
VirajithaDilsh <2021sp169@univ.jfn.ac.lk>
2025-08-01 16:29:00 +0530

    Update page.tsx

--- src/app/animals/new-animal/page.tsx
+++ src/app/animals/new-animal/page.tsx
@@ -1,11 +1,14 @@
+import { Suspense } from "react";
 import AnimalEntryForm from "@/components/AnimalEntryForm";
 
 export default function NewAnimalPage() {
-
     return (
         <div className="min-h-screen bg-gray-100 p-6">
             <h1 className="text-3xl text-black font-bold mb-6">Add New Animal</h1>
-            <AnimalEntryForm />
+
+            <Suspense fallback={<div>Loading form...</div>}>
+                <AnimalEntryForm />
+            </Suspense>
         </div>
     );
 }
