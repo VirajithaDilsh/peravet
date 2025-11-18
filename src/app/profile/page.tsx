607fae865fefa98ea3ee27cfcf999ac43f955a4d
VirajithaDilsh <2021sp169@univ.jfn.ac.lk>
2025-11-18 21:46:37 +0530

    profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,6 @@
+"use client";
+import ProfileSidebar from "@/components/ProfileSidebar";
+
+export default function ProfilePage() {
+    return <ProfileSidebar isOpen={true} onClose={() => {}} />;
+}
