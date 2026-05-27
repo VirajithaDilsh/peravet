a4cbd849ad55d13d66284b0059f28de41890f5ac
Virajitha Dilshan <virajitha.Jayathilaka@gmail.com>
2026-05-27 21:32:53 +0530

    student page update

--- src/app/dashboard/students/page.tsx
+++ src/app/dashboard/students/page.tsx
@@ -2,20 +2,23 @@
 
 import StudentTable from "@/components/tables/StudentTable";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import { sampleUsers } from "@/types/users";
+import { useUserContext } from "@/context/UserContext";
 
-export default function Home() {
-    // Only pass students to the table
-    const studentUsers = sampleUsers.filter(user => user.role === "student");
+export default function StudentsPage() {
+  const { users } = useUserContext();
 
-    return (
-        <ProtectedRoute allowedRoles={["admin","doctor"]}>
-            <div className="flex">
-                <main className="flex-1 p-4 text-black">
-                    <h1 className="text-2xl font-semibold mb-4">Students Records</h1>
-                    <StudentTable users={studentUsers} />
-                </main>
-            </div>
-        </ProtectedRoute>
-    );
+  // Only pass students to the table
+  const studentUsers = users.filter((user) => user.role === "student");
+
+  return (
+    <ProtectedRoute allowedRoles={["admin", "doctor"]}>
+      <div className="flex">
+        <main className="flex-1 p-4 text-black">
+          <h1 className="text-2xl font-semibold mb-4">Students Records</h1>
+
+          <StudentTable users={studentUsers} />
+        </main>
+      </div>
+    </ProtectedRoute>
+  );
 }
